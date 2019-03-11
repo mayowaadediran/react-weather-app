5e062ceb0ed83fcafefd09efbd598b61f08fbbf6
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-03-12 00:21:20 +0100

    add data for states selection

---
+++ src/statesData.js
@@ -0,0 +1,375 @@
+const statesData = [ {
+    "id": 2337542,
+    "name": "Imo State",
+    "country": "NG",
+    "coord": {
+      "lon": 7.16667,
+      "lat": 5.5
+    }
+  },
+{
+    "id": 2597367,
+    "name": "Yobe State",
+    "country": "NG",
+    "coord": {
+      "lon": 11.5,
+      "lat": 12
+    }
+  },
+{
+    "id": 2347468,
+    "name": "Bauchi State",
+    "country": "NG",
+    "coord": {
+      "lon": 10,
+      "lat": 10.5
+    }
+  },
+{
+    "id": 2595349,
+    "name": "Zamfara State",
+    "country": "NG",
+    "coord": {
+      "lon": 6.25,
+      "lat": 12.16667
+    }
+  },
+{
+    "id": 2328926,
+    "name": "Federal Republic of Nigeria",
+    "country": "NG",
+    "coord": {
+      "lon": 8,
+      "lat": 10
+    }
+  },
+{
+    "id": 2335722,
+    "name": "Kaduna State",
+    "country": "NG",
+    "coord": {
+      "lon": 7.75,
+      "lat": 10.33333
+    }
+  },
+{
+    "id": 2332453,
+    "name": "Lagos State",
+    "country": "NG",
+    "coord": {
+      "lon": 3.75,
+      "lat": 6.58333
+    }
+  },
+{
+    "id": 2347266,
+    "name": "Benue State",
+    "country": "NG",
+    "coord": {
+      "lon": 8.75,
+      "lat": 7.33333
+    }
+  },
+
+{
+    "id": 2326171,
+    "name": "Ondo",
+    "country": "NG",
+    "coord": {
+      "lon": 4.83333,
+      "lat": 7.1
+    }
+  },
+{
+    "id": 2324433,
+    "name": "Rivers State",
+    "country": "NG",
+    "coord": {
+      "lon": 6.82766,
+      "lat": 4.74974
+    }
+  },
+{
+    "id": 2325190,
+    "name": "Oyo State",
+    "country": "NG",
+    "coord": {
+      "lon": 4,
+      "lat": 8
+    }
+  },
+
+{
+    "id": 2565344,
+    "name": "Enugu State",
+    "country": "NG",
+    "coord": {
+      "lon": 7.5,
+      "lat": 6.5
+    }
+  },
+{
+    "id": 2332785,
+    "name": "Kwara State",
+    "country": "NG",
+    "coord": {
+      "lon": 5,
+      "lat": 8.5
+    }
+  },
+
+{
+    "id": 2335204,
+    "name": "Kano",
+    "country": "NG",
+    "coord": {
+      "lon": 8.51672,
+      "lat": 12.00012
+    }
+  },
+
+{
+    "id": 2565343,
+    "name": "Edo",
+    "country": "NG",
+    "coord": {
+      "lon": 6,
+      "lat": 6.5
+    }
+  },
+
+{
+    "id": 2327546,
+    "name": "Ogun State",
+    "country": "NG",
+    "coord": {
+      "lon": 3.58333,
+      "lat": 7
+    }
+  },
+
+{
+    "id": 2597365,
+    "name": "Osun State",
+    "country": "NG",
+    "coord": {
+      "lon": 4.5,
+      "lat": 7.5
+    }
+  },
+{
+    "id": 2326168,
+    "name": "Ondo State",
+    "country": "NG",
+    "coord": {
+      "lon": 5.08333,
+      "lat": 7.16667
+    }
+  },
+
+{
+    "id": 2347470,
+    "name": "Bauchi",
+    "country": "NG",
+    "coord": {
+      "lon": 9.84327,
+      "lat": 10.31344
+    }
+  },
+{
+    "id": 2595347,
+    "name": "Gombe State",
+    "country": "NG",
+    "coord": {
+      "lon": 11.16667,
+      "lat": 10.25
+    }
+  },
+{
+    "id": 2345891,
+    "name": "Cross River State",
+    "country": "NG",
+    "coord": {
+      "lon": 8.5,
+      "lat": 5.75
+    }
+  },
+
+{
+    "id": 2334797,
+    "name": "Katsina State",
+    "country": "NG",
+    "coord": {
+      "lon": 7.5,
+      "lat": 12.25
+    }
+  },
+
+{
+    "id": 2597364,
+    "name": "Kogi State",
+    "country": "NG",
+    "coord": {
+      "lon": 6.75,
+      "lat": 7.75
+    }
+  },
+{
+    "id": 2346794,
+    "name": "Borno State",
+    "country": "NG",
+    "coord": {
+      "lon": 13,
+      "lat": 11.5
+    }
+  },
+{
+    "id": 2328925,
+    "name": "Niger State",
+    "country": "NG",
+    "coord": {
+      "lon": 6,
+      "lat": 10
+    }
+  },
+
+{
+    "id": 2349961,
+    "name": "Anambra State",
+    "country": "NG",
+    "coord": {
+      "lon": 7,
+      "lat": 6.33333
+    }
+  },
+{
+    "id": 2322907,
+    "name": "Sokoto State",
+    "country": "NG",
+    "coord": {
+      "lon": 5.25,
+      "lat": 13.08333
+    }
+  },
+{
+    "id": 2565341,
+    "name": "Delta State",
+    "country": "NG",
+    "coord": {
+      "lon": 6,
+      "lat": 5.5
+    }
+  },
+{
+    "id": 2565342,
+    "name": "Adamawa State",
+    "country": "NG",
+    "coord": {
+      "lon": 12.5,
+      "lat": 9.33333
+    }
+  },
+{
+    "id": 2324828,
+    "name": "Plateau State",
+    "country": "NG",
+    "coord": {
+      "lon": 9.75,
+      "lat": 9.16667
+    }
+  },
+
+{
+    "id": 2350813,
+    "name": "Akwa Ibom State",
+    "country": "NG",
+    "coord": {
+      "lon": 7.83333,
+      "lat": 5
+    }
+  },
+{
+    "id": 2565340,
+    "name": "Abia State",
+    "country": "NG",
+    "coord": {
+      "lon": 7.5,
+      "lat": 5.41667
+    }
+  },
+{
+    "id": 2595346,
+    "name": "Ekiti State",
+    "country": "NG",
+    "coord": {
+      "lon": 5.25,
+      "lat": 7.66667
+    }
+  },
+{
+    "id": 2352776,
+    "name": "Abuja Federal Capital Territory",
+    "country": "NG",
+    "coord": {
+      "lon": 7.16667,
+      "lat": 8.83333
+    }
+  },
+
+{
+    "id": 2595344,
+    "name": "Bayelsa State",
+    "country": "NG",
+    "coord": {
+      "lon": 6.08333,
+      "lat": 4.75
+    }
+  },
+{
+    "id": 2595348,
+    "name": "Nassarawa State",
+    "country": "NG",
+    "coord": {
+      "lon": 8.25,
+      "lat": 8.5
+    }
+  },
+{
+    "id": 2597366,
+    "name": "Taraba State",
+    "country": "NG",
+    "coord": {
+      "lon": 10.5,
+      "lat": 8
+    }
+  },
+{
+    "id": 2595345,
+    "name": "Ebonyi State",
+    "country": "NG",
+    "coord": {
+      "lon": 8.08333,
+      "lat": 6.25
+    }
+  },
+{
+    "id": 2565345,
+    "name": "Jigawa State",
+    "country": "NG",
+    "coord": {
+      "lon": 9.75,
+      "lat": 12
+    }
+  },
+{
+    "id": 2319480,
+    "name": "Uyo",
+    "country": "NG",
+    "coord": {
+      "lon": 7.92657,
+      "lat": 5.03333
+    }
+  }
+]
+
+export default statesData
