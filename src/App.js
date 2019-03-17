54db19eff0c55a751f59a399ef46201e0c64a5d1
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-03-17 22:04:48 +0100

    add states and event handlers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import CurrentWeather from './Components/CurrentWeather/CurrentWeather';
-import LocationSelector from './Components/ForecastWeather/ForecastWeather';
-import ForecastWeather from './Components/ForecastWeather/ForecastWeather'
+import LocationSelector from './Components/LocationSelector/LocationSelector';
+import ForecastWeather from './Components/ForecastWeather/ForecastWeather';
+import locationData from './locationData'
 
 import './App.css';
 
 class App extends Component {
-  
+  state = {
+    locationData : locationData,
+    selectedLocation: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      selectedLocation: e.target.value
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert("you selected" + this.state.selectedLocation);
+    console.log(this.state.LocationData)
+  }
+
   render() {
     return (
       <div className="App">
         <NavBar />
-        <LocationSelector />
+        <LocationSelector 
+          locationData = {this.state.locationData} 
+          selectedLocation = {this.state.selectedLocation}
+          handleChange = {this.handleChange}
+          handleSubmit = {this.handleSubmit}/>
         <CurrentWeather />
         <ForecastWeather />
-        <h1>Weather</h1>
       </div>
     );
   }
