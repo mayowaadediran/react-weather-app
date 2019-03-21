504ff0f84f06afe8af89975505fa40023321c684
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-03-21 15:12:59 +0100

    add about component

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import About from '../About/About';
+import './NavBar.css'
 
 
 const NavBar = () => {
   return (
     <nav>
-      <h1>Weather</h1>
-      <p>Weather for locations in Nigeria</p>
+      <h1>Weather Now Now</h1>
+      <p>Weather conditions for locations in Nigeria</p>
+      <About />
     </nav>
   );
 }
